b795667486d88a5f074e6557b38ca1f318c1c0cf
Cleef Souza <a.cleef.souza@gmail.com>
2020-09-25 04:08:44 -0300

    Tipos primitivos

---
+++ primeiros-passos/tipos.php
@@ -0,0 +1,15 @@
+<?php
+
+$idade = 25;
+$nome = "Cleef";
+$salario = 10500.30;
+$casado = true;
+
+// echo gettype($nome); // Output: string
+
+var_dump([
+    $idade,
+    $nome,
+    $salario,
+    $casado
+]);
