7fcba9a4b6e4554a24cfba3c0e25c75e9a118f59
Cleef Souza <a.cleef.souza@gmail.com>
2020-09-25 04:08:30 -0300

    Operações

---
+++ primeiros-passos/operacoes.php
@@ -0,0 +1,13 @@
+<?php
+
+$idade = 25;
+$idadeDaqui10Anos = $idade + 10;
+$idadeHa5Anos = $idade - 5;
+$idadePelaMetade = $idade / 2;
+$idadeDobrada = $idade * 2;
+
+echo "Idade: {$idade}\n";
+echo "Daqui a 10 anos: {$idadeDaqui10Anos}\n";
+echo "Há 5 anos: {$idadeHa5Anos}\n";
+echo "Pela metade: {$idadePelaMetade}\n";
+echo "Dobrada: {$idadeDobrada}";
