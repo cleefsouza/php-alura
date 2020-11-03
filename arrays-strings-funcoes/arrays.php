1711c8b344daafdb949a0940abb9c23340b2026c
Cleef Souza <a.cleef.souza@gmail.com>
2020-11-02 22:05:20 -0300

    Destruct

--- arrays-strings-funcoes/arrays.php
+++ arrays-strings-funcoes/arrays.php
@@ -3,6 +3,9 @@
 $nomes = array("João", "Maria", "Carlos", "Pedro", "Luana", "Lais");
 $idades = [21, 25, 30, 48, 60, 16, 12, 19, 80];
 
+[$joao, $maria, $carlos] = $idades;
+var_dump($joao, $maria, $carlos);
+
 for ($i = 0; $i < count($idades); $i++) {
     echo $idades[$i] . PHP_EOL;
 }
