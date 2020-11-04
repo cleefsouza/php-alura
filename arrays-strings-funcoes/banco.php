46b8acd7a053dbf8d302e9bd52d9b533554a2748
Cleef Souza <a.cleef.souza@gmail.com>
2020-11-03 22:13:21 -0300

    Remover itens

--- arrays-strings-funcoes/banco.php
+++ arrays-strings-funcoes/banco.php
@@ -1,23 +1,32 @@
 <?php
 
-require_once ("funcoes.php");
+require_once("funcoes.php");
 
-$conta = [
-    "cpf" => "123.456.789-10",
-    "titular" => "Cleef Souza",
-    "saldo" => 5000
-
+$contas = [
+    "01-0" => [
+        "cpf" => "123.456.789-10",
+        "titular" => "Cleef Souza",
+        "saldo" => 5000
+    ],
+    "02-0" => [
+        "cpf" => "109.876.543-12",
+        "titular" => "João Agripino",
+        "saldo" => 600
+    ]
 ];
 
 // execução
 
-exibir($conta);
+exibir($contas["01-0"]);
 
-$conta = sacar(5000, $conta);
-exibir($conta);
+$contas["01-0"] = sacar(5000, $contas["01-0"]);
+exibir($contas["01-0"]);
 
-$conta = sacar(1000, $conta);
-exibir($conta);
+$contas["02-0"] = sacar(1000, $contas["02-0"]);
+exibir($contas["02-0"]);
 
-$conta = depositar(2.50, $conta);
-exibir($conta);
+$contas["02-0"] = depositar(2.50, $contas["02-0"]);
+exibir($contas["02-0"]);
+
+remover($contas, "01-0");
+exibir($contas["01-0"] ?? null);
--- arrays-strings-funcoes/funcoes.php
+++ arrays-strings-funcoes/funcoes.php
@@ -1,6 +1,7 @@
 <?php
 
 const ZERADO = 0;
+const DIVISOR = "------------------------" . PHP_EOL;
 
 // sub-rotinas
 
@@ -10,19 +11,25 @@ const ZERADO = 0;
  */
 function msg(string $acao, bool $sucesso = false): void
 {
-    echo $sucesso ? "Ação ($acao) realizada com sucesso." . PHP_EOL : "Você não pode $acao esse valor." . PHP_EOL;
+    echo $sucesso
+        ? "Ação ($acao) realizada com sucesso." . PHP_EOL . DIVISOR
+        : "Você não pode $acao esse valor." . PHP_EOL . DIVISOR;
 }
 
 /**
- * @param array $conta
+ * @param array|null $conta
  */
-function exibir(array $conta): void
+function exibir(?array $conta): void
 {
-    echo "------------------------" . PHP_EOL;
+    if (empty($conta)) {
+        msg("exibir");
+        return;
+    }
+
     echo "CPF: {$conta["cpf"]}" . PHP_EOL;
     echo "Titular: {$conta["titular"]}" . PHP_EOL;
     echo "Saldo: R$ {$conta["saldo"]}" . PHP_EOL;
-    echo "------------------------" . PHP_EOL;
+    echo DIVISOR;
 }
 
 // funções
@@ -61,4 +68,19 @@ function depositar(float $valor, array $conta): array
     msg("depositar", true);
 
     return $conta;
+}
+
+/**
+ * @param array $contas
+ * @param string $numero
+ */
+function remover(array &$contas, string $numero): void
+{
+    if (key_exists($numero, $contas)) {
+        unset($contas[$numero]);
+        msg("remover, conta $numero", true);
+        return;
+    }
+
+    msg("remover");
 }
