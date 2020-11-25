f6cd4cde79d0be8cde606195d59d7c3a7f298dca
Aryosvalldo Cleef de Souza <aryosvalldo@voxtecnologia.com.br>
2020-11-25 11:45:03 -0300

    Finally

--- php-exceptions/MinhaException.php
+++ php-exceptions/MinhaException.php
@@ -11,7 +11,15 @@ class MinhaException extends DomainException
 }
 
 try {
+    echo "Start \o" . PHP_EOL;
     throw new MinhaException();
 } catch (MinhaException $exp) {
-    $exp->showExp(); // output: Errou!
+    $exp->showExp();
+} finally {
+    echo "Sempre executo o/" . PHP_EOL;
 }
+
+// output:
+// Start \o
+// Errou!
+// Sempre executo o/
