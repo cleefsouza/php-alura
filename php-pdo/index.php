79caa7d4f3c94e15f13014dbb51b0f788d3a92c7
Aryosvalldo Cleef de Souza <aryosvalldo@voxtecnologia.com.br>
2020-12-14 12:26:20 -0300

    Delete aluno

--- php-pdo/index.php
+++ php-pdo/index.php
@@ -10,11 +10,22 @@ $pdo = new PDO("sqlite:" . $path);
 $aluno = new Aluno(null, "Cleef Souza", new DateTime("1995-07-03"));
 print_r($aluno);
 
-$insert = "INSERT INTO aluno (nome, nascimento) VALUES ('{$aluno->getNome()}', '{$aluno->getNascimento()->format("Y-m-d")}')";
+$insert = "INSERT INTO aluno (nome, nascimento) VALUES (:nome, :nascimento)";
 print_r($insert . PHP_EOL);
-$pdo->exec($insert);
+
+$stm = $pdo->prepare($insert);
+$stm->bindValue("nome", $aluno->getNome());
+$stm->bindValue("nascimento", $aluno->getNascimento()->format("Y-m-d"));
+$stm->execute();
 
 $stm = $pdo->query("SELECT * FROM aluno;");
 $resul = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($resul);
+
+$delete = "DELETE FROM aluno WHERE id = ?";
+$stm = $pdo->prepare($delete);
+$stm->bindValue(1, 5, PDO::PARAM_INT);
+$stm->execute();
 
 print_r($resul);
