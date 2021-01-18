0cc7cf9d5c16a0cd9e5e0ae3bf4953c167821912
Aryosvalldo Cleef de Souza <aryosvalldo@voxtecnologia.com.br>
2021-01-18 13:05:40 -0300

    Page not found

---
+++ mvc/view/erro404.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Cursos Alura</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous">
+</head>
+<body>
+    <div class="vh-100 d-flex justify-content-center align-items-center">
+        <h2>Página não encontrada: <span class="text-danger">404</span></h2>
+    </div>
+</body>
+</html>
